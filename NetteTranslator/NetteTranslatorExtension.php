42e7a371347544c670984da2f8dbe83caa02ded2
Schmutzka <tomas.vot@gmail.com>
2013-03-26 04:12:46 +0100

    Refactoring: extension options extended

--- NetteTranslator/NetteTranslatorExtension.php
+++ NetteTranslator/NetteTranslatorExtension.php
@@ -1,19 +1,20 @@
 <?php
-/**
- * Author: Paulík Miroslav
- * Date:   16.3.13
- */
 
 namespace NetteTranslator;
 
+use Nette;
 
-class NetteTranslatorExtension extends \Nette\Config\CompilerExtension
+class NetteTranslatorExtension extends Nette\Config\CompilerExtension
 {
-	public $defaults = array(
-		'lang'      => 'en',
-		'cacheMode' => Gettext::CACHE_DISABLE,
-		'files'     => array()
+	/** @var array */
+	private $defaults = array(
+		'lang' => 'en',
+		'files' => array(),
+		'useCache' => FALSE,
+		'layout' => 'horizontal',
+		'height' => 450
 	);
+
 
 	public function loadConfiguration()
 	{
@@ -21,15 +22,15 @@ class NetteTranslatorExtension extends \Nette\Config\CompilerExtension
 		$builder = $this->getContainerBuilder();
 
 		$translator = $builder->addDefinition($this->prefix('translator'));
-		$translator->setClass('NetteTranslator\Gettext', array(
-															  '@session',
-															  '@cacheStorage',
-															  '@httpResponse'
-														 ));
+
+		$translator->setClass('NetteTranslator\Gettext', array('@session', '@cacheStorage', '@httpResponse'));
 		$translator->addSetup('setLang', $config['lang']);
+		$translator->addSetup('setCacheUse', $config['useCache']);
 		foreach ($config["files"] as $id => $file) {
 			$translator->addSetup('addFile', $file, $id);
 		}
-		$translator->addSetup('NetteTranslator\Panel::register');
+
+		$translator->addSetup('NetteTranslator\Panel::register', array('@application', '@self', '@session', '@httpRequest', $config['layout'], $config['height']));
 	}
+
 }
